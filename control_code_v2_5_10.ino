6f576afe22fa422859338f0de6717c4e2c7af045
Zhongwen Zhang <24zhangzhongwen@gmail.com>
2025-05-10 18:20:14 -0700

    control_code_v2_5_10.ino
    car exhibits noticeable correction behavior, will "zig zag", alternating between left and right.

---
+++ control_code_v2_5_10.ino
@@ -0,0 +1,75 @@
+#include "ECE3.h"
+
+uint16_t sensorValues[8]; 
+
+const int left_nslp_pin = 31;
+const int left_dir_pin = 29;
+const int left_pwm_pin = 40;
+
+const int right_nslp_pin = 11;
+const int right_dir_pin = 30;
+const int right_pwm_pin = 39;
+
+bool print_directions = true;
+const int LED_RF = 41;
+const int num_samples = 5;
+
+///////////////////////////////////
+void setup() {
+  pinMode(left_nslp_pin, OUTPUT);
+  pinMode(left_dir_pin, OUTPUT);
+  pinMode(left_pwm_pin, OUTPUT);
+  digitalWrite(left_dir_pin, LOW);
+  digitalWrite(left_nslp_pin, HIGH);
+
+  pinMode(right_nslp_pin, OUTPUT);
+  pinMode(right_dir_pin, OUTPUT);
+  pinMode(right_pwm_pin, OUTPUT);
+  digitalWrite(right_dir_pin, LOW);
+  digitalWrite(right_nslp_pin, HIGH);
+
+  pinMode(LED_RF, OUTPUT);
+
+  ECE3_Init();
+
+  Serial.begin(9600); 
+  delay(2000);
+
+  int leftSpd = 70;
+  int rightSpd = 70;
+  analogWrite(left_pwm_pin, leftSpd);
+  analogWrite(right_pwm_pin, rightSpd);
+}
+
+/////////////////////////////////////////////
+// Weighted error calculation
+////////////////////////////////////////////
+
+int weightScheme_8421(const uint16_t n[8]){
+  return (-8*n[0] - 4*n[1] - 2*n[2] - n[3] + n[4] + 2*n[5] + 4*n[6] + 8*n[7]) / 4;  
+}
+
+int getError() {  
+  int mins[8] = {667, 575, 553, 691, 553, 599, 621, 714};
+  int maxs[8] = {1833, 1925, 1947, 1807, 1947, 1901, 1879, 1786};
+
+  ECE3_read_IR(sensorValues);
+
+
+  for (int i = 0; i < 8; i++) {
+    sensorValues[i] -= mins[i]; // zero the data 
+    sensorValues[i] *= 1000; // normalize the data to have max of 1000
+    sensorValues[i] /= maxs[i];
+  }
+
+  return weightScheme_8421(sensorValues);
+}
+
+/////////////////////////////////////////////
+// Main loop
+////////////////////////////////////////////
+
+void loop() {
+  int error = getError(); // get the sensor readings and convert them into an error term 
+  adjustWheelSpeed(error); // adjust the wheel speed in response to the error term
+}
